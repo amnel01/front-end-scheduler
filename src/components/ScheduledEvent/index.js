b53d57bac26fe8c1137777864a74f405e44b7b1f
Alex Nelson <anelson@ancestry.com>
2018-11-03 14:34:10 -0600

    scheduled event component

--- src/components/ScheduledEvent/index.js
+++ src/components/ScheduledEvent/index.js
@@ -3,10 +3,24 @@ import propTypes from 'prop-types';
 
 import './style.css';
 
-const ScheduledEvent = () => <div className="scheduled-event">Hello World</div>;
+const ScheduledEvent = ({ eventName, coachName, datetime }) => (
+  <div className="scheduled-event">
+    <div className="datetime">
+      <h3 className="time">630am</h3>
+      <h4 className="day">Mon</h4>
+    </div>
 
-ScheduledEvent.propTypes = {};
+    <div className="content">
+      <h2 className="event-name">{eventName}</h2>
+      <div className="coach-name">Coach {coachName}</div>
+    </div>
+  </div>
+);
 
-ScheduledEvent.defaultProps = {};
+ScheduledEvent.propTypes = {
+  eventName: propTypes.string.isRequired,
+  coachName: propTypes.string.isRequired,
+  datetime: propTypes.number.isRequired
+};
 
 export default ScheduledEvent;
--- src/components/ScheduledEvent/index.story.js
+++ src/components/ScheduledEvent/index.story.js
@@ -1,7 +1,43 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text, number } from '@storybook/addon-knobs';
+import '../../index.css';
 
 import ScheduledEvent from '.';
 
-storiesOf('ScheduledEvent', module).add('default', () => <ScheduledEvent />);
+storiesOf('ScheduledEvent', module)
+  .addDecorator(withKnobs)
+
+  .addDecorator(story => (
+    <div style={{ padding: 16, width: 500, height: 100 }}>{story()}</div>
+  ))
+
+  .add('single', () => (
+    <ScheduledEvent
+      eventName={text('Event Name', 'This is a really long event name')}
+      coachName={text('Coach', 'Andy Copandy')}
+      datetime={number('DateTime', new Date().getTime())}
+    />
+  ))
+
+  .add('list', () => (
+    <React.Fragment>
+      <ScheduledEvent
+        eventName={text('Event Name', 'This is a really long event name')}
+        coachName={text('Coach', 'Andy Copandy')}
+        datetime={number('DateTime', new Date().getTime())}
+      />
+
+      <ScheduledEvent
+        eventName={text('Event Name', 'This is a really long event name')}
+        coachName={text('Coach', 'Andy Copandy')}
+        datetime={number('DateTime', new Date().getTime())}
+      />
+
+      <ScheduledEvent
+        eventName={text('Event Name', 'This is a really long event name')}
+        coachName={text('Coach', 'Andy Copandy')}
+        datetime={number('DateTime', new Date().getTime())}
+      />
+    </React.Fragment>
+  ));
